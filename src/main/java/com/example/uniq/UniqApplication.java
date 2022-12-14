8eb9f02435427102171eece88d27d5a8385aa50e
App Masters <AppMasters@MacBook-Pro-de-Baraky.local>
2022-12-14 11:30:59 -0300

    first commit

---
+++ src/main/java/com/example/uniq/UniqApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqApplication.class, args);
+	}
+
+}
